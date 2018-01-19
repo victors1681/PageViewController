f1df5f748d83f53b48991e5306b0ad4a4ecf4686
Victor Santos <victor.santos@ipsoft.com>
2018-01-19 17:27:20 -0500

    first commit

---
+++ Autolayout/PageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PageCollectionViewCell.swift
+//  Autolayout
+//
+//  Created by Victor Santos on 1/19/18.
+//  Copyright © 2018 itsoluclick. All rights reserved.
+//
+
+import UIKit
+
+class PageCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Autolayout/SwipeController+CollectionView.swift
@@ -0,0 +1,9 @@
+//
+//  SwipeController+CollectionView.swift
+//  Autolayout
+//
+//  Created by Victor Santos on 1/19/18.
+//  Copyright © 2018 itsoluclick. All rights reserved.
+//
+
+import Foundation
